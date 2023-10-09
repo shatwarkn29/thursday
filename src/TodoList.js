5a784930b1e76b5f55537039787d576054f68f3a
Sumukha Hatwar K N <109708558+shatwarkn29@users.noreply.github.com>
2023-10-09 14:44:55 +0530

    SEC-6 Update TodoList.js Done.

--- src/TodoList.js
+++ src/TodoList.js
@@ -19,7 +19,7 @@ function TodoList() {
 
   return (
     <div className='todolist'>
-      <h1>Todo List Daily </h1>
+      <h1>Todo List </h1>
       <div className='thing'>
         <input
           type="text"
